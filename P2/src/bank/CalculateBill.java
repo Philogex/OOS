ee690670a3cc186cd29439cc277d50180f071d08
Philogex <64011915+Philogex@users.noreply.github.com>
2023-10-30 09:58:02 +0100

    adjusted UML diagram to account for previous changes added javadoc for CaclulateBill interface

--- P2/src/bank/CalculateBill.java
+++ P2/src/bank/CalculateBill.java
@@ -1,7 +1,11 @@
 package bank;
 /**
- *
+ * Interface includes functions for caluculating a bill of a Transaction
  */
 public interface CalculateBill {
+    /**
+     * calculates amount of Transaction with taxes, etc.
+     * @return amount of Transaction with deductions
+     */
     double calculate();
 }
